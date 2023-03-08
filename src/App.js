714122521bbb70c9c1b26a33869074ecac4cee7a
leekyoungsil <ooowdu@gmail.com>
2023-03-08 22:30:33 +0900

    버튼

--- src/App.js
+++ src/App.js
@@ -4,28 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload2.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React wandu !!!
-        </a>
-      </header> */}
-      <div className='Div1'>
-        <div className="Div_1"></div>
-        <div className='Div_2'></div>
+      <div className="header">
+        <button className='btn'>완두</button>
+        <button className='btn'>땅콩</button>
+        <button className='btn'>사랑</button>
+        <button className='btn'>킁킁</button>
+        <button className='btn'>쫑빈</button>
       </div>
-      <div className='Div2'>
-        <div className='Div_3'></div>
-        <div className='Div_4'></div>
-      </div>
+      <div className='contents'></div>
+      <div className='footer'></div>
     </div>
   );
 }
